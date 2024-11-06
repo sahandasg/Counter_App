c340e8ea9b308a7f2e1def9babdf800e95b419b3
sahand <sahandasg56@gmail.com>
2024-11-06 09:21:20 +0330

    develop CustomCounter.jsx without css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import CustomCounter from "./components/CustomCounter";
 
 
 function App() {
   return (
-    <div>hello world</div>
+    <CustomCounter/>
   );
 }
 
---
+++ src/components/CustomCounter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function CustomCounter() {
+  return (
+    <div className="container">
+      <h1 id="num">0</h1>
+      <div className="btns">
+        <button className="dec">
+          <i className="fas fa-minus"></i>
+        </button>
+        <button className="reset">
+          <i className="fas fa-redo"></i>
+        </button>
+        <button className="inc">
+          <i className="fas fa-plus"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
